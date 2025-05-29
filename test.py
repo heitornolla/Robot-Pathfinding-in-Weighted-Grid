31f7a604336ab4043d4dd49776fd3c3dea5c48ff
heitor <heitor.nolla@gmail.com>
2025-05-29 14:24:33 -0300

    refactored testing script

--- test.py
+++ test.py
@@ -1,44 +1,52 @@
 import random
-
-from src.utils import print_path_with_open
 random.seed(42)
 
 from src.algorithms.a_star import a_star
+from src.algorithms.bfs import breadth_first_search
+from src.algorithms.dfs import depth_first_search
 from src.algorithms.greedy_best_first import greedy_best_first
+
 from src.grid.grid import Grid
+from src.utils import print_path_with_open
 
 
 # Explain on 5x5 grid, demonstrate on 20x20 and 50x50
 # Showcase open nodes while debugging the code
 
-# Add more heuristics to show differences - BFS, DFS, overestimation
+terrain_types = ['W', 'G', 'M', 'T']
+terrain_weights = [0.1, 0.4, 0.25, 0.15]  
+
+terrain_map = [
+    [random.choices(terrain_types, terrain_weights)[0] for _ in range(50)]
+    for _ in range(50)
+]
+
+grid = Grid(terrain_map)
+start = (0, 0)
+goal = (38, 46)
+
+
+def test_algorithms(grid, start, goal):
+    algorithms = {
+        "A*": a_star,
+        "Greedy Best-First": greedy_best_first,
+        "Breadth-First Search": breadth_first_search,
+        "Depth-First Search": depth_first_search
+    }
+
+    results = {}
+    for name, algo in algorithms.items():
+        path, cost, opened = algo(grid, start, goal)
+        results[name] = {
+            "path_length": len(path),
+            "cost": cost,
+            "nodes_opened": len(opened)
+        }
+    return results
+
+
 if __name__ == "__main__":
-    terrain_types = ['W', 'G', 'M', 'T']
-    terrain_weights = [0.1, 0.4, 0.25, 0.15]  
+    results = test_algorithms(grid, start, goal)
 
-    terrain_map = [
-        [random.choices(terrain_types, terrain_weights)[0] for _ in range(30)]
-        for _ in range(30)
-    ]
-
-    grid = Grid(terrain_map)
-    start = (0, 0)
-    goal = (26, 16)
-
-    path, cost, opened_nodes = a_star(grid, start, goal)
-
-    print('')
-    
-    print_path_with_open(grid, path, opened_nodes)
-    print(f"\nTotal cost: {cost:.2f}")
-    print(f"Path length: {len(path) if path else 0}")
-    print(f"Opened {len(opened_nodes)} nodes during the search.")
-
-    path, cost, opened_nodes = greedy_best_first(grid, start, goal)
-
-    print('')
-    
-    print_path_with_open(grid, path, opened_nodes)
-    print(f"\nTotal cost: {cost:.2f}")
-    print(f"Path length: {len(path) if path else 0}")
-    print(f"Opened {len(opened_nodes)} nodes during the search.")
+    for name, data in results.items():
+        print(f"{name}: Cost={data['cost']:.2f}, Path Length={data['path_length']}, Opened={data['nodes_opened']}")
